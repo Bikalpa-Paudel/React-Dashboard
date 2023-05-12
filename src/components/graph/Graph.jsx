ac67937887eb01c1531d0acdf4b0ec4938736a6e
Bikalpa <bikalpapaudel53@gmail.com>
2023-05-12 19:52:32 +0545

    addind react chart with Rechart package

--- src/components/graph/Graph.jsx
+++ src/components/graph/Graph.jsx
@@ -1,9 +1,73 @@
-import './graph.css'
+import "./graph.css";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
-export default function Graph(){
-    return(
-        <div className="graph">
-            graph
-        </div>
-    )
+export default function Graph() {
+  const data = [
+    {
+      name: "January",
+      Total: 3000,
+    },
+    {
+      name: "March",
+      Total: 5000,
+    },
+    {
+      name: "May",
+      Total: 2500,
+    },
+    {
+      name: "July",
+      Total: 2008,
+    },
+    {
+      name: "September",
+      Total: 3500,
+    },
+    {
+      name: "November",
+      Total: 3800,
+    },
+
+  ];
+  return (
+    <div className="graph">
+      <div className="graph-title">Years Revenue</div>
+      <ResponsiveContainer width="100%" aspect={2 / .8}>
+        <AreaChart
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#82ca9d"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 }
